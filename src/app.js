3fbe20e5571797aa9ca91aef8967298517e8e52a
hiranmoyee <hiranmoyeedehingia@gmail.com>
2023-11-15 22:52:39 +0530

    Init commit

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const hbs = require('hbs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
+
 // console.log(__dirname)
 // console.log(path.join(__dirname, '../public')) //manipulation of paths of directories
 
